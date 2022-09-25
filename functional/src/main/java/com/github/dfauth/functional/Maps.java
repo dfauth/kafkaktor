fd8b3d66f06e144dbe523d263324f093604dcfc5
damian <dfauth@gmail.com>
2022-09-25 09:23:27 +0800

    refactoring

--- functional/src/main/java/com/github/dfauth/functional/Maps.java
+++ functional/src/main/java/com/github/dfauth/functional/Maps.java
@@ -2,8 +2,10 @@ package com.github.dfauth.functional;
 
 import java.util.Map;
 import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Maps {
 
@@ -17,6 +19,16 @@ public class Maps {
 
     public static <T, V, K> Function<Map.Entry<K,V>, T> mapEntryTransformer(BiFunction<K,V,T> f) {
         return e -> f.apply(e.getKey(), e.getValue());
+    }
+
+    @SafeVarargs
+    public static <K, V> Map<K,V> concat(Map<K, V>... maps) {
+        return concat((v1,v2) -> v2, maps);
+    }
+
+    @SafeVarargs
+    public static <K, V> Map<K,V> concat(BinaryOperator<V> mergeFunction, Map<K, V>... maps) {
+        return Stream.of(maps).flatMap(m -> m.entrySet().stream()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, mergeFunction));
     }
 
 }
