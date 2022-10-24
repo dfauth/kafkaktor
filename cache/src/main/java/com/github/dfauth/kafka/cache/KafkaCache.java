c5af703400a2a9cfaa5c4ece4cdced5f098a1bf9
damian <dfauth@gmail.com>
2022-10-25 07:52:14 +0800

    computeIfPresent, cacheMiss

--- cache/src/main/java/com/github/dfauth/kafka/cache/KafkaCache.java
+++ cache/src/main/java/com/github/dfauth/kafka/cache/KafkaCache.java
@@ -4,6 +4,7 @@ import com.github.dfauth.kafka.RebalanceListener;
 import com.github.dfauth.kafka.StreamBuilder;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -13,68 +14,80 @@ import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
+import static com.github.dfauth.functional.Functions.peek;
+import static com.github.dfauth.functional.Try.tryWithCallable;
 import static com.github.dfauth.kafka.RebalanceListener.noOp;
 
-public class KafkaCache<K, V, T, R> {
+public class KafkaCache<K, V, T, R, S> {
 
     private final StreamBuilder<K, V, T, R> builder;
-    private final Cache<T, R> cache;
+    private final Cache<T, S> cache;
     private final BiConsumer<T, R> messageConsumer;
     private StreamBuilder.KafkaStream<K, V> stream;
+    private BiFunction<S,R,S> computeIfPresent;
+    private Function<T,S> computeIfAbsent;
 
-    public KafkaCache(StreamBuilder<K, V, T, R> builder, Cache<T, R> cache, BiConsumer<T, R> messageConsumer, RebalanceListener<K,V> partitionAssignmentConsumer, RebalanceListener<K,V> partitionRevocationConsumer) {
+    public KafkaCache(StreamBuilder<K, V, T, R> builder, Cache<T, S> cache, BiConsumer<T, R> messageConsumer, RebalanceListener<K,V> partitionAssignmentConsumer, RebalanceListener<K,V> partitionRevocationConsumer, BiFunction<S,R,S> computeIfPresent, Function<T,S> computeIfAbsent) {
         this.builder = builder;
         this.cache = cache;
         this.messageConsumer = messageConsumer;
         this.builder.onPartitionAssignment(partitionAssignmentConsumer);
         this.builder.onPartitionRevocation(partitionRevocationConsumer);
+        this.computeIfPresent = computeIfPresent;
+        this.computeIfAbsent = computeIfAbsent;
     }
 
-    public static <K, V, T, R> Builder<K, V, T, R> builder() {
+    public static <K, V, T, R, S> Builder<K, V, T, R, S> builder() {
         return new Builder<>();
     }
 
-    public static Builder<String,String,String,String> stringBuilder() {
-        return new Builder<String,String,String,String>()
+    public static Builder<String,String,String,String,String> stringBuilder() {
+        return new Builder<String,String,String,String,String>()
                 .withKeyMapper((k,v) -> k)
                 .withKeyDeserializer(new StringDeserializer())
                 .withValueDeserializer(new StringDeserializer())
                 .withValueMapper((k,v)->v);
     }
 
-    public static <K, V, R> Builder<K, V, K, R> unmappedKeyBuilder() {
-        return new Builder<K, V, K, R>()
+    public static <K, V, R, S> Builder<K, V, K, R, S> unmappedKeyBuilder() {
+        return new Builder<K, V, K, R, S>()
                 .withKeyMapper((k,v) -> k);
     }
 
-    public static <V, R> Builder<String, V, String, R> unmappedStringKeyBuilder() {
-        return new Builder<String, V, String, R>()
+    public static <V, R, S> Builder<String, V, String, R, S> unmappedStringKeyBuilder() {
+        return new Builder<String, V, String, R, S>()
                 .withKeyMapper((k,v) -> k)
                 .withKeyDeserializer(new StringDeserializer());
     }
 
-    public static <V, R> Builder<Long, V, Long, R> unmappedLongKeyBuilder() {
-        return new Builder<Long, V, Long, R>()
+    public static <V, R, S> Builder<Long, V, Long, R, S> unmappedLongKeyBuilder() {
+        return new Builder<Long, V, Long, R, S>()
                 .withKeyMapper((k,v) -> k)
                 .withKeyDeserializer(new LongDeserializer());
     }
 
-    public static <K, V, T> Builder<K, V, T, V> unmappedValueBuilder() {
-        return new Builder<K, V, T, V>()
+    public static <K, V, T, S> Builder<K, V, T, V, S> unmappedValueBuilder() {
+        return new Builder<K, V, T, V, S>()
                 .withValueMapper((k,v) -> v);
     }
 
-    public static <K, V> Builder<K, V, K, V> unmappedBuilder() {
-        return new Builder<K, V, K, V>()
+    public static <K, V, S> Builder<K, V, K, V, S> unmappedBuilder() {
+        return new Builder<K, V, K, V, S>()
                 .withKeyMapper((k,v) -> k)
                 .withValueMapper((k,v) -> v);
     }
 
     public void start() {
-        this.stream = this.builder.withKeyValueConsumer((k, v) -> {
-            cache.put(k, v);
-            this.messageConsumer.accept(k, cache.getIfPresent(k));
+        this.stream = this.builder.withKeyValueConsumer((t, r) -> {
+            cache.asMap()
+                    .compute(t,
+                            (k,v) -> Optional.ofNullable(v)
+                                    .map(oldV -> computeIfPresent.apply(oldV, r))
+                                    .orElseGet(() -> computeIfPresent.apply(tryWithCallable(() -> computeIfAbsent.apply(t)).toOptional().orElse(null),r))
+                    );
+            this.messageConsumer.accept(t, r);
         }).build();
         this.stream.start();
     }
@@ -83,79 +96,102 @@ public class KafkaCache<K, V, T, R> {
         this.stream.stop();
     }
 
-    public Optional<R> getOptional(T t) {
-        return Optional.ofNullable(cache.getIfPresent(t));
-    }
-
-    public static class Builder<K, V, T, R> {
+    public static class Builder<K, V, T, R, S> {
 
         private final StreamBuilder<K, V, T, R> streamBuilder = StreamBuilder.builder();
-        private final CacheBuilder<Object, Object> cacheBuilder = CacheBuilder.newBuilder();
         private BiFunction<K, V, T> keyMapper = (k,v) -> (T)k;
         private BiFunction<K, V, R> valueMapper = (k,v) -> (R)v;
         private RebalanceListener<K,V> partitionAssignmentConsumer = noOp();
         private RebalanceListener<K,V> partitionRevocationConsumer = noOp();
         private BiConsumer<T, R> messageConsumer = (k,v) -> {};
+        private Consumer<CacheBuilder<Object, Object>> cacheConfigurer = b -> {};
+        private BiFunction<S,R,S> computeIfPresent = (oldR, newR) -> (S)newR;
+        private Function<T, S> cacheMiss = t -> {
+            throw new IllegalArgumentException("no cache value for key "+t);
+        };
 
-        public KafkaCache<K, V, T, R> build() {
 
+        public KafkaCache<K, V, T, R, S> build() {
+
+            CacheLoader<T,S> cacheLoader = new CacheLoader<T, S>() {
+                @Override
+                public S load(T t) {
+                    return cacheMiss.apply(t);
+                }
+            };
             return new KafkaCache<>(
                     streamBuilder.withKeyMapper(k -> keyMapper.apply(k, null)).withValueMapper(v -> valueMapper.apply(null, v)),
-                    cacheBuilder.build(),
+                    peek(cacheConfigurer).apply(CacheBuilder.newBuilder()).build(cacheLoader),
                     messageConsumer,
                     partitionAssignmentConsumer,
-                    partitionRevocationConsumer
+                    partitionRevocationConsumer,
+                    computeIfPresent,
+                    cacheMiss  // computeIfAbsent
             );
         }
 
-        public KafkaCache.Builder<K, V, T, R> withKeyDeserializer(Deserializer<K> keyDeserializer) {
+        private R create(T t) {
+            return null;
+        }
+
+        public KafkaCache.Builder<K, V, T, R, S> withKeyDeserializer(Deserializer<K> keyDeserializer) {
             streamBuilder.withKeyDeserializer(keyDeserializer);
             return this;
         }
 
-        public KafkaCache.Builder<K, V, T, R> withValueDeserializer(Deserializer<V> valueDeserializer) {
+        public KafkaCache.Builder<K, V, T, R, S> withValueDeserializer(Deserializer<V> valueDeserializer) {
             streamBuilder.withValueDeserializer(valueDeserializer);
             return this;
         }
 
         @SafeVarargs
-        public final Builder<K, V, T, R> withProperties(Map<String, Object>... configs) {
+        public final Builder<K, V, T, R, S> withProperties(Map<String, Object>... configs) {
             streamBuilder.withProperties(configs);
             return this;
         }
 
-        public Builder<K, V, T, R> withTopic(String topic) {
+        public Builder<K, V, T, R, S> withTopic(String topic) {
             streamBuilder.withTopic(topic);
             return this;
         }
 
-        public Builder<K, V, T, R> withCacheConfiguration(Consumer<CacheBuilder<Object, Object>> consumer) {
-            consumer.accept(cacheBuilder);
+        public Builder<K, V, T, R, S> withCacheConfiguration(Consumer<CacheBuilder<Object, Object>> cacheConfigurer) {
+            this.cacheConfigurer = cacheConfigurer;
             return this;
         }
 
-        public Builder<K, V, T, R> withKeyMapper(BiFunction<K,V,T> keyMapper) {
+        public Builder<K, V, T, R, S> onCacheMiss(Function<T,S> cacheMiss) {
+            this.cacheMiss = cacheMiss;
+            return this;
+        }
+
+        public Builder<K, V, T, R, S> withKeyMapper(BiFunction<K,V,T> keyMapper) {
             this.keyMapper = keyMapper;
             return this;
         }
 
-        public Builder<K, V, T, R> withValueMapper(BiFunction<K,V,R> valueMapper) {
+        public Builder<K, V, T, R, S> withValueMapper(BiFunction<K,V,R> valueMapper) {
             this.valueMapper = valueMapper;
             return this;
         }
 
-        public Builder<K, V, T, R> onPartitionAssignment(RebalanceListener<K,V> consumer) {
+        public Builder<K, V, T, R, S> onPartitionAssignment(RebalanceListener<K,V> consumer) {
             this.partitionAssignmentConsumer = consumer;
             return this;
         }
 
-        public Builder<K, V, T, R> onPartitionRevocation(RebalanceListener<K,V> consumer) {
+        public Builder<K, V, T, R, S> onPartitionRevocation(RebalanceListener<K,V> consumer) {
             this.partitionRevocationConsumer = consumer;
             return this;
         }
 
-        public Builder<K, V, T, R> onMessage(BiConsumer<T, R> consumer) {
+        public Builder<K, V, T, R, S> onMessage(BiConsumer<T, R> consumer) {
             this.messageConsumer = consumer;
+            return this;
+        }
+
+        public Builder<K, V, T, R, S> computeIfPresent(BiFunction<S,R,S> computeIfPresent) {
+            this.computeIfPresent = computeIfPresent;
             return this;
         }
     }
--- cache/src/test/java/com/github/dfauth/kafka/cache/CacheTest.java
+++ cache/src/test/java/com/github/dfauth/kafka/cache/CacheTest.java
@@ -1,9 +1,14 @@
 package com.github.dfauth.kafka.cache;
 
+import com.github.dfauth.functional.Lists;
 import com.github.dfauth.kafka.KafkaSink;
+import com.github.dfauth.kafka.assertion.Assertions;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -18,6 +23,7 @@ public class CacheTest {
     public static final String TOPIC = "topic";
     public static final String K = "key";
     public static final String V = "value";
+    public static final String V1 = "value1";
     private static final int PARTITIONS = 1;
 
     @Test
@@ -26,7 +32,7 @@ public class CacheTest {
                 .withPartitions(PARTITIONS)
                 .withGroupId("blah")
                 .runAsyncTest(f -> config -> {
-                    KafkaCache<String, String, String, String> cache = KafkaCache.stringBuilder()
+                    KafkaCache<String, String, String, String, String> cache = KafkaCache.stringBuilder()
                             .withProperties(config)
                             .withTopic(TOPIC)
                             .onPartitionAssignment(seekToBeginning())
@@ -43,6 +49,40 @@ public class CacheTest {
                     assertNotNull(m);
                 });
         assertEquals(V, value.get());
+    }
+
+    @Test
+    public void testCollection() throws ExecutionException, InterruptedException {
+        CompletableFuture<Assertions> value = withEmbeddedKafka()
+                .withPartitions(PARTITIONS)
+                .withGroupId("blah")
+                .runAsyncTest(f -> config -> {
+                    Assertions.Builder assertionBuilder = Assertions.builder();
+                    Assertions.OptionalQueue<CompletableFuture<Object>> f1 = assertionBuilder.assertThat(a1 -> assertEquals(List.of(V), a1), a2 -> assertEquals(List.of(V, V1), a2));
+                    assertionBuilder.build(f);
+                    KafkaCache<String, String, String, String, List<String>> cache = KafkaCache.<String, String, List<String>>unmappedStringKeyBuilder()
+                            .withValueDeserializer(new StringDeserializer())
+                            .withProperties(config)
+                            .withTopic(TOPIC)
+                            .onPartitionAssignment(seekToBeginning())
+                            .onCacheMiss(k -> new ArrayList<>())
+                            .computeIfPresent((oldV, newV) -> {
+                                List<String> result = Lists.concat(oldV, newV);
+                                f1.poll().ifPresent(_f -> _f.complete(result));
+                                return result;
+                            })
+                            .build();
+
+                    cache.start();
+
+                    KafkaSink<String, String> sink = KafkaSink.newStringBuilder()
+                            .withProperties(config)
+                            .withTopic(TOPIC)
+                            .build();
+                    assertNotNull(sink.publish(K, V).get(1000, TimeUnit.MILLISECONDS));
+                    assertNotNull(sink.publish(K, V1).get(1000, TimeUnit.MILLISECONDS));
+                });
+        value.get().performAssertions();
     }
 
 }
